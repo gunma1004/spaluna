d2ebc7f39d9bd8d2930aac0c59bb62132ea3db36
gunma1004 <hanabank94@naver.com>
2026-08-19 14:06:36 +0900

    인천 및 경기 전 지역 구/동 세부 바로가기 버튼 UI 연결 배포

---
+++ add_dong_grid_capital.py
@@ -0,0 +1,275 @@
+﻿import os
+import re
+
+DETAILED_REGIONS = {
+    # -------------------------------------------------------------
+    # [인천광역시 8개 구/군 & 세부 동]
+    # -------------------------------------------------------------
+    "incheon_bupyeong": ("인천 부평구", [
+        ("bupyeong", "부평동"), ("sanggok", "산곡동"), ("cheongcheon", "청천동"),
+        ("galsan", "갈산동"), ("sipjeong", "십정동"), ("bugae", "부개동"), ("samsan", "삼산동")
+    ]),
+    "incheon_namdong": ("인천 남동구", [
+        ("guwol", "구월동"), ("ganseok", "간석동"), ("mansu", "만수동"),
+        ("nonhyeon_incheon", "논현동"), ("seochang", "서창동"), ("dorim", "도림동")
+    ]),
+    "incheon_yeonsu": ("인천 연수구", [
+        ("songdo", "송도동"), ("yeonsu", "연수동"), ("dongchun", "동춘동"),
+        ("cheonghak", "청학동"), ("okryeon", "옥련동"), ("seonhak", "선학동")
+    ]),
+    "incheon_michuhol": ("인천 미추홀구", [
+        ("juan", "주안동"), ("yonghyeon", "용현동"), ("hakik", "학익동"),
+        ("dohwa", "도화동"), ("sungui", "숭의동"), ("gwangyo", "관교동")
+    ]),
+    "incheon_seogu": ("인천 서구", [
+        ("cheongna", "청라동"), ("geomdan", "검단"), ("luwon", "루원시티"),
+        ("gajeong", "가정동"), ("seoknam", "석남동"), ("yeonhui", "연희동"),
+        ("dangha", "당하동"), ("majeon", "마전동")
+    ]),
+    "incheon_gyeyang": ("인천 계양구", [
+        ("gyeyang", "계양"), ("jakjeon", "작전동"), ("hyoseong", "효성동"),
+        ("gyesan", "계산동"), ("seoun", "서운동")
+    ]),
+    "incheon_junggu": ("인천 중구", [
+        ("yeongjong", "영종도"), ("unseo", "운서동"), ("jungsan", "중산동"),
+        ("sinpo", "신포동"), ("dongincheon", "동인천")
+    ]),
+    "incheon_donggu": ("인천 동구", [
+        ("songhyeon", "송현동"), ("songrim", "송림동"), ("manseok", "만석동"), ("hwasu", "화수동")
+    ]),
+
+    # -------------------------------------------------------------
+    # [수원시 4개 구 & 세부 동]
+    # -------------------------------------------------------------
+    "suwon_paldal": ("수원 팔달구", [
+        ("ingye", "인계동"), ("haenggung", "행궁동"), ("hwaseo", "화서동"), ("ji-dong", "지동"), ("maesan", "매산동")
+    ]),
+    "suwon_yeongtong": ("수원 영통구", [
+        ("gwanggyo", "광교"), ("yeongtong", "영통동"), ("mangpo", "망포동"), ("maetan", "매탄동"), ("woncheon", "원천동")
+    ]),
+    "suwon_jangan": ("수원 장안구", [
+        ("jeongja", "정자동"), ("jo-won", "조원동"), ("yuljeon", "율전동"), ("cheoncheon", "천천동"), ("yeonmu", "연무동")
+    ]),
+    "suwon_gwonseon": ("수원 권선구", [
+        ("gwonseon", "권선동"), ("gosaek", "고색동"), ("homaesil", "호매실동"), ("seriu", "세류동"), ("geumgok", "금곡동")
+    ]),
+
+    # -------------------------------------------------------------
+    # [성남시 3개 구 & 세부 동]
+    # -------------------------------------------------------------
+    "seongnam_bundang": ("성남 분당구", [
+        ("seohyeon", "서현동"), ("yatap", "야탑동"), ("jeongja", "정자동"), ("pangyo", "판교"),
+        ("baekhyeon", "백현동"), ("sunae", "수내동"), ("ime", "이매동"), ("gumi", "구미동"), ("unjoong", "운중동")
+    ]),
+    "seongnam_sujeong": ("성남 수정구", [
+        ("wirye", "위례"), ("sinheung", "신흥동"), ("taepyeong", "태평동"),
+        ("sanseong", "산성동"), ("bokjeong", "복정동"), ("sujin", "수진동")
+    ]),
+    "seongnam_jungwon": ("성남 중원구", [
+        ("moran", "모란"), ("seongnam_dong", "성남동"), ("sangdaewon", "상대원동"),
+        ("hagdaewon", "하대원동"), ("geumgwang", "금광동"), ("bank", "은행동")
+    ]),
+
+    # -------------------------------------------------------------
+    # [고양시 3개 구 & 세부 동]
+    # -------------------------------------------------------------
+    "goyang_ilsandong": ("고양 일산동구", [
+        ("baekseok", "백석동"), ("madu", "마두동"), ("janghang", "장항동"),
+        ("jeongbalsan", "정발산동"), ("siksa", "식사동"), ("pungsan", "풍산동")
+    ]),
+    "goyang_ilsanseo": ("고양 일산서구", [
+        ("juyeop", "주엽동"), ("daehwa", "대화동"), ("tanhyun", "탄현동"),
+        ("ilsan", "일산동"), ("songsan", "송산동"), ("deogi", "덕이동")
+    ]),
+    "goyang_deogyang": ("고양 덕양구", [
+        ("hwajeong", "화정동"), ("haengsin", "행신동"), ("samsong", "삼송"),
+        ("wonheung", "원흥"), ("hyangdong", "향동"), ("deogeun", "덕은"), ("wondang", "원당")
+    ]),
+
+    # -------------------------------------------------------------
+    # [용인시 3개 구 & 세부 동]
+    # -------------------------------------------------------------
+    "yongin_suji": ("용인 수지구", [
+        ("pungdeokcheon", "풍덕천동"), ("jookjeon", "죽전동"), ("dongcheon", "동천동"),
+        ("sanghyeon", "상현동"), ("shinbong", "신봉동"), ("sungbok", "성복동")
+    ]),
+    "yongin_giheung": ("용인 기흥구", [
+        ("dongbaek", "동백동"), ("singal", "신갈동"), ("gugal", "구갈동"),
+        ("bora", "보라동"), ("seonong", "서농동"), ("guseong", "구성"), ("mabuk", "마북동")
+    ]),
+    "yongin_cheoin": ("용인 처인구", [
+        ("kimryangjang", "김량장동"), ("yeokbuk", "역북동"), ("samga", "삼가동"),
+        ("pogok", "포곡"), ("mohan", "모현"), ("yangji", "양지")
+    ]),
+
+    # -------------------------------------------------------------
+    # [안양시 / 안산시 & 세부 동]
+    # -------------------------------------------------------------
+    "anyang_dongan": ("안양 동안구", [
+        ("pyeongchon", "평촌동"), ("beomgye", "범계"), ("indeogwon", "인덕원"),
+        ("gwanyang", "관양동"), ("hogye", "호계동"), ("bisan", "비산동")
+    ]),
+    "anyang_manan": ("안양 만안구", [
+        ("anyang_dong", "안양동"), ("seoksu", "석수동"), ("bakdal", "박달동")
+    ]),
+    "ansan_danwon": ("안산 단원구", [
+        ("gojan", "고잔동"), ("jungang", "중앙동"), ("chogi", "초지동"),
+        ("wongok", "원곡동"), ("seonbu", "선부동"), ("daebu", "대부도")
+    ]),
+    "ansan_sangnok": ("안산 상록구", [
+        ("bono", "본오동"), ("sadong", "사동"), ("wolpi", "월피동"),
+        ("seongpo", "성포동"), ("il-dong", "일동"), ("i-dong", "이동")
+    ]),
+
+    # -------------------------------------------------------------
+    # [경기 주요 시 단위 & 세부 동]
+    # -------------------------------------------------------------
+    "bucheon": ("부천시", [
+        ("jungdong", "중동"), ("sangdong", "상동"), ("sinjungdong", "신중동"),
+        ("sosa", "소사동"), ("wonmi", "원미동"), ("ojeong", "오정동"), ("yeokgok", "역곡동"), ("gogang", "고강동")
+    ]),
+    "hwaseong": ("화성시", [
+        ("dongtan", "동탄1"), ("dongtan2", "동탄2"), ("byeongjeom", "병점"),
+        ("hyangnam", "향남"), ("bongdam", "봉담"), ("namyang", "남양"), ("saesol", "새솔동"), ("jinjoo", "진안동")
+    ]),
+    "pyeongtaek": ("평택시", [
+        ("godeok", "고덕"), ("bijeon", "비전동"), ("songtan", "송탄"),
+        ("anjeong", "안정리"), ("anseok", "안중"), ("poseung", "포승"), ("cheongbuk", "청북"), ("sejeong", "세교동")
+    ]),
+    "siheung": ("시흥시", [
+        ("baegot", "배곧동"), ("jeongwang", "정왕동"), ("eunhaeng", "은행동"),
+        ("mokgam", "목감동"), ("daeya", "대야동"), ("sinhyeon", "신현동"), ("neunggok", "능곡동"), ("janghyeon", "장현동")
+    ]),
+    "gimpo": ("김포시", [
+        ("gurae", "구래동"), ("unyang", "운양동"), ("janggi", "장기동"),
+        ("pungmu", "풍무동"), ("sau", "사우동"), ("masan", "마산동"), ("gochon", "고촌"), ("tongjin", "통진")
+    ]),
+    "paju": ("파주시", [
+        ("unjeong", "운정"), ("geumchon", "금촌동"), ("munsan", "문산"),
+        ("gyoha", "교하"), ("yadang", "야당동"), ("dongpae", "동패동")
+    ]),
+    "namyangju": ("남양주시", [
+        ("dasang", "다산동"), ("byeolnae", "별내동"), ("pyeongnae", "평내동"),
+        ("hopyeong", "호평동"), ("jinjeop", "진접"), ("wabu", "와부"), ("onam", "오남"), ("hwado", "화도")
+    ]),
+    "uijeongbu": ("의정부시", [
+        ("uijeongbu_dong", "의정부동"), ("howon", "호원동"), ("singok", "신곡동"),
+        ("minrak", "민락동"), ("gosan", "고산동"), ("ganeung", "가능동"), ("geumo", "금오동")
+    ]),
+    "hanam": ("하남시", [
+        ("misa", "미사"), ("wirye_hanam", "위례"), ("gamil", "감일"),
+        ("deokpung", "덕풍동"), ("sinjang", "신장동"), ("pungcheon", "풍산동")
+    ]),
+    "gwangmyeong": ("광명시", [
+        ("cheolsan", "철산동"), ("gwangmyeong_dong", "광명동"), ("soha", "소하동"), ("iljik", "일직동"), ("haan", "하안동")
+    ]),
+    "gunpo": ("군포시", [
+        ("sanbon", "산본동"), ("geumjeong", "금정동"), ("dang-dong", "당동"), ("daeyami", "대야미"), ("bugok", "부곡동")
+    ]),
+    "guri": ("구리시", [
+        ("sutaek", "수택동"), ("inmae", "인창동"), ("galmae", "갈매동"), ("gyomun", "교문동"), ("achasan", "아천동")
+    ]),
+    "osan": ("오산시", [
+        ("won-dong", "원동"), ("seggyo", "세교"), ("gweol", "궐동"), ("osandong", "오산동"), ("eunjeong", "은계동")
+    ]),
+    "gwangju_gyeonggi": ("경기 광주시", [
+        ("gyeongan", "경안동"), ("taejeon", "태전동"), ("opocheup", "오포"),
+        ("sinhyun", "신현동"), ("neungpyeong", "능평동"), ("tanbeol", "탄벌동")
+    ]),
+    "icheon": ("이천시", [
+        ("changjeon", "창전동"), ("jeungpo", "증포동"), ("bubal", "부발"), ("majung", "마장"), ("anheung", "안흥동")
+    ]),
+    "yangju": ("양주시", [
+        ("okjeong", "옥정동"), ("goeup", "고읍동"), ("deokgye", "덕계동"), ("baekseok_yangju", "백석")
+    ]),
+    "uiwang": ("의왕시", [
+        ("poil", "포일동"), ("naeson", "내손동"), ("gojeon", "고천동"), ("sam-dong", "삼동")
+    ]),
+    "anseong": ("안성시", [
+        ("gongdo", "공도"), ("daedeok", "대덕"), ("anseong_dong", "안성동"), ("boggae", "보개")
+    ])
+}
+
+# 시 단위 메인 폴더 (수원, 성남, 고양, 용인, 안양, 안산)에서 구/동 링크 연결
+CITY_MAP = {
+    "suwon": ("수원시", [
+        ("../suwon_paldal/index.html", "팔달구"), ("../suwon_yeongtong/index.html", "영통구"),
+        ("../suwon_jangan/index.html", "장안구"), ("../suwon_gwonseon/index.html", "권선구")
+    ]),
+    "seongnam": ("성남시", [
+        ("../seongnam_bundang/index.html", "분당구"), ("../seongnam_sujeong/index.html", "수정구"),
+        ("../seongnam_jungwon/index.html", "중원구")
+    ]),
+    "goyang": ("고양시", [
+        ("../goyang_ilsandong/index.html", "일산동구"), ("../goyang_ilsanseo/index.html", "일산서구"),
+        ("../goyang_deogyang/index.html", "덕양구")
+    ]),
+    "yongin": ("용인시", [
+        ("../yongin_suji/index.html", "수지구"), ("../yongin_giheung/index.html", "기흥구"),
+        ("../yongin_cheoin/index.html", "처인구")
+    ]),
+    "anyang": ("안양시", [
+        ("../anyang_dongan/index.html", "동안구"), ("../anyang_manan/index.html", "만안구")
+    ]),
+    "ansan": ("안산시", [
+        ("../ansan_danwon/index.html", "단원구"), ("../ansan_sangnok/index.html", "상록구")
+    ])
+}
+
+count = 0
+
+# 1. 경기/인천 각 구별 index.html 및 세부 동 html 에 동 바로가기 그리드 삽입
+for folder, (kr_name, dongs) in DETAILED_REGIONS.items():
+    if not os.path.exists(folder):
+        continue
+    
+    grid_items = "".join([f'<a href="{f}.html" class="gu-item">{name}</a>\n            ' for f, name in dongs])
+    gu_grid_html = f"""
+        <h2 class="section-title">{kr_name} 세부 동 바로가기</h2>
+        <div class="gu-grid">
+            {grid_items}
+        </div>
+    """
+
+    for file in os.listdir(folder):
+        if not file.endswith(".html"):
+            continue
+        file_path = os.path.join(folder, file)
+        
+        with open(file_path, "r", encoding="utf-8") as f:
+            content = f.read()
+
+        # gu-grid 가 이미 있으면 교체, 없으면 container 닫기 직전에 추가
+        if '<div class="gu-grid">' in content:
+            content = re.sub(r'<h2 class="section-title">.*?세부.*?</h2>\s*<div class="gu-grid">.*?</div>', gu_grid_html.strip(), content, flags=re.DOTALL)
+        else:
+            content = re.sub(r'(</div>\s*<footer>)', rf'{gu_grid_html}\n    \1', content, flags=re.DOTALL)
+
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(content)
+        count += 1
+
+# 2. 수원/성남/고양 등 시 단위 대표 폴더 index.html 에 하위 구 바로가기 그리드 삽입
+for city_folder, (city_kr, gus) in CITY_MAP.items():
+    if not os.path.exists(city_folder):
+        continue
+    grid_items = "".join([f'<a href="{link}" class="gu-item">{name}</a>\n            ' for link, name in gus])
+    city_grid_html = f"""
+        <h2 class="section-title">{city_kr} 전체 구 바로가기</h2>
+        <div class="gu-grid">
+            {grid_items}
+        </div>
+    """
+    file_path = os.path.join(city_folder, "index.html")
+    if os.path.exists(file_path):
+        with open(file_path, "r", encoding="utf-8") as f:
+            content = f.read()
+        if '<div class="gu-grid">' in content:
+            content = re.sub(r'<h2 class="section-title">.*?전체 구.*?</h2>\s*<div class="gu-grid">.*?</div>', city_grid_html.strip(), content, flags=re.DOTALL)
+        else:
+            content = re.sub(r'(</div>\s*<footer>)', rf'{city_grid_html}\n    \1', content, flags=re.DOTALL)
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(content)
+        count += 1
+
+print(f"🎉 총 {count}개 경기/인천 전체 페이지에 구/동 바로가기 버튼 UI가 완벽하게 연결되었습니다.")
